04e3217eea4fd6e6d05b36c155e23b7dc9d7bfae
Ian <ianpante15@gmail.com>
2022-03-08 20:56:05 +0800

    Add AboutMe project

---
+++ AboutMe/CareerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by macbook on 3/8/22.
+//
+
+import UIKit
+
+class CareerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
